26c22492b8965f213cb8d5b7c6dd0149c73df86a
EmmanuelAraque <164089877+EmmanuelAraque@users.noreply.github.com>
2024-06-18 21:04:01 -0300

    Create listas-y-objetos.js

---
+++ tp-10/listas-y-objetos.js
@@ -0,0 +1,132 @@
+var listaPersonasEjemplo = [
+    {
+        "apellido": "Perez",
+        "nombre": "Juan",
+        "edad": 20,
+        "documento": 12345
+    },
+    {
+        "apellido": "Lopez",
+        "nombre": "Luis",
+        "edad": 20,
+        "documento": 23456
+    },
+    {
+        "apellido": "Zapata",
+        "nombre": "Pablo",
+        "edad": 10,
+        "documento": 34567
+    },
+    {
+        "apellido": "Acuña",
+        "nombre": "Ana",
+        "edad": 30,
+        "documento": 45678
+    },
+];
+
+/**
+ * 01 - ordenarPorApellido
+ * 
+ * Recibe
+ * - `listaDePersonas`: una lista, array, con objetos de la forma `persona`.
+ * 
+ * Retorna: 
+ * - el mismo listado, ordenado alfabéticamente por el apellido de la persona 
+ */
+function ordenarPorApellido(listaDePersonas) {
+    let OrdenoApellido = listaDePersonas.sort(function (a, b){
+        return a.apellido.localeCompare(b.apellido);
+     });
+
+     return OrdenoApellido;
+}
+console.log("ordenarPorApellido()", ordenarPorApellido(listaPersonasEjemplo));
+
+/**
+ * 02 - soloNombres
+ * 
+ * Recibe
+ * - `listaDePersonas`: una lista, array, con objetos de la forma `persona`
+ * 
+ * Retorna: 
+ * - una lista de strings, con sólo los nombres de las personas
+ */
+function soloNombres(listaDePersonas) {
+    let nombre = [];
+    for (let persona of listaDePersonas){
+        nombre.push(persona.nombre)
+    }
+    return nombre;
+}
+console.log("soloNombres()", soloNombres(listaPersonasEjemplo));
+
+/**
+ * 03 - promedioEdades
+ * 
+ * Recibe
+ * - `listaDePersonas`: una lista, array, con objetos de la forma `persona`
+ * 
+ * Retorna: 
+ * - un numero, con el cálculo del promedio de las edades
+ */
+function promedioEdades(listaDePersonas) {
+    let ResultadoProm = 0;
+    let sumaEdades = 0;
+    for (let persona of listaDePersonas) {
+        sumaEdades += persona.edad;
+    }
+    let promedio = sumaEdades / listaDePersonas.length;
+
+    // retornamos al promedio 
+    return promedio;
+
+    
+}
+
+console.log("promedioEdades()", promedioEdades(listaPersonasEjemplo));
+
+/**
+ * 04 - soloMayoresDeEdad
+ * 
+ * Recibe
+ * - `listaDePersonas`: una lista, array, con objetos de la forma `persona`
+ * 
+ * Retorna: 
+ * - una lista, array, conteniendo solamente las personas con más de 18 años
+ */
+function soloMayoresDeEdad(listaDePersonas) {
+    let PersonaMayor = [];
+    for (let persona of listaDePersonas){
+        if (persona.edad > 18){
+            PersonaMayor.push(persona)
+        }
+    };
+    return PersonaMayor;
+}
+console.log("soloMayoresDeEdad()", soloMayoresDeEdad(listaPersonasEjemplo));
+
+/**
+ * 05 - laPersonaMayor
+ * 
+ * Recibe
+ * - `listaDePersonas`: una lista, array, con objetos de la forma `persona`
+ * 
+ * Retorna: 
+ * - una objeto con la persona de mayor edad en todo el listado. 
+ * En caso de que hayan 2 personas con la misma edad, se puede retornar la primera que aparezca en el listado.
+ */
+function laPersonaMayor(listaDePersonas) {
+    let PersonaMayor;
+    let edadGrande = 0 ;
+
+    for( let persona of listaDePersonas){
+        if (persona.edad > edadGrande) {
+            edadGrande = persona.edad
+            PersonaMayor = persona
+        }
+    }
+
+    return PersonaMayor;
+}
+console.log("laPersonaMayor()", laPersonaMayor(listaPersonasEjemplo));
